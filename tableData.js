d4de5f1a1866d4f87d11e487272696021d33c038
wcollins185 <wcollins185@gmail.com>
2018-11-28 21:15:35 -0800

    saving table Arrays

--- tableData.js
+++ tableData.js
@@ -1,1 +1,10 @@
-blah
+var tableRes = [
+  {
+    resName: "John Doe",
+    phoneNumber:"999-999-9999",
+    resEmail: "JohnDoe@gmail.com",
+    resID: "JD999",
+  }
+];
+
+module.exports = tableRes;
